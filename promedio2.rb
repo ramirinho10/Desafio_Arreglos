b50a953aebd45ed42ad120b7dc7a5dc1842148ff
Ramiro <r.silva.pedreros@gmail.com>
2021-07-12 22:41:08 -0400

    Se arregla promedio2.rb

--- promedio2.rb
+++ promedio2.rb
@@ -1,12 +1,19 @@
 a = [1,2,3,4,5]
 b = [5,6,7,8,9]
 
-def compara(array1, array2)
-    if array1.sum/array1.count > array2.sum/array2.count
-        puts array1.sum/array1.count
+def promedio(arreglo)
+    arreglo.sum / arreglo.count
+end
+
+
+def compara_arrays(array1, array2)
+    promedio1 = promedio(array1)
+    promedio2 = promedio(array2)
+    if promedio1 > promedio2
+        promedio
     else
-        puts array2.sum/array2.count
+        promedio2
     end
 end
 
-print compara(a,b)
+puts compara_arrays(a,b)
